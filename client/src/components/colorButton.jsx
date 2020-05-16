c27f0fb3edfdf1201279e552320896aa14241b9f
alephshahor <adrianalvarezleon@gmail.com>
2020-05-16 21:05:36 +0100

    Eslint set up

--- client/src/components/colorButton.jsx
+++ client/src/components/colorButton.jsx
@@ -1,18 +1,17 @@
 import React from 'react'
-import "../stylesheets/colorButton.css"
+import '../stylesheets/colorButton.css'
 
-/*Icons made by <a href="https://www.flaticon.com/authors/vectors-market" 
-title="Vectors Market">Vectors Market</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>*/
+/* Icons made by <a href="https://www.flaticon.com/authors/vectors-market"
+title="Vectors Market">Vectors Market</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> */
 
-export default function ColorButton(props) {
-
-    const colorName = props.colorName;
-    return (
-        <div id="color-button-wrapper">
-            <button id="color-button" type="button" onClick={() => props.changeSelectedColor(props.colorCode)}>
-                <span id="color-text">{colorName}</span>
-            </button>
-            <img id="color-image" src={require('../images/colors/' + colorName + ".svg")} alt="Color"/>
-        </div>
-    )
+export default function ColorButton (props) {
+  const colorName = props.colorName
+  return (
+    <div id="color-button-wrapper">
+      <button id="color-button" type="button" onClick={() => props.changeSelectedColor(props.colorCode)}>
+        <span id="color-text">{colorName}</span>
+      </button>
+      <img id="color-image" src={require('../images/colors/' + colorName + '.svg')} alt="Color"/>
+    </div>
+  )
 }
