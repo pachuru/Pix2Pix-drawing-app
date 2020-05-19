17d54fbe0ba1d1262cd4c15adddd9b06b7101d07
alephshahor <adrianalvarezleon@gmail.com>
2020-05-19 18:47:30 +0100

    Created ExecutionHistory class

---
+++ client/src/utils/executionHistory.js
@@ -0,0 +1,46 @@
+
+
+export default class ExecutionHistory {
+
+    constructor(){
+        this.history =  []
+        this.historyMaxLength = 4
+        this.currentSnapshotIndex = -1
+    }
+
+    push = (element) => {
+        if(this.currentSnapshotIndex >= (this.historyMaxLength - 1)){
+            this.popBack()
+            this.history[this.currentSnapshotIndex] = element
+        }else{
+          this.currentSnapshotIndex += 1
+          this.history[this.currentSnapshotIndex] = element
+        }
+    }
+
+    undo = () => {
+        if(this.currentSnapshotIndex >= 1){
+           this.currentSnapshotIndex -= 1
+           return this.history[this.currentSnapshotIndex]
+        }else return null;
+    }
+
+    redo = () => {
+        if(this.currentSnapshotIndex <= (this.historyMaxLength - 2)){
+            this.currentSnapshotIndex += 1
+            return this.history[this.currentSnapshotIndex]
+        }else return null;
+    }
+
+    
+    popBack = () => {
+        let newHistory = []
+        for(var i = 1; i < this.historyMaxLength; i++){
+            newHistory.push(this.history[i])
+        }
+        newHistory.push(null)
+        this.history = newHistory;
+        console.log(this.history)
+    }
+
+}
