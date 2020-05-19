9aa4d5fd77ffa4a4936c0affc18e9fc88c1368a7
alephshahor <adrianalvarezleon@gmail.com>
2020-05-19 18:48:25 +0100

    Created utils folder and arraysAreEqual function

---
+++ client/src/utils/index.js
@@ -0,0 +1,17 @@
+
+export const utils = {
+        arraysAreEqual(arr1, arr2){
+
+        if(arr1.length !== arr2.length)
+            return false
+
+        for(let i = 0; i < arr1.length; i++){
+            if(arr1[i] !== arr2[i])
+                return false
+        }
+
+        return true
+    }
+}
+
+export default utils;
