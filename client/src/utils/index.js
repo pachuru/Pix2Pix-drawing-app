e98e6d42555911dd1f6abcf0a7e4ac4a32b054c1
alephshahor <adrianalvarezleon@gmail.com>
2020-05-20 00:08:48 +0100

    Created isPointInsideRectangle function

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,17 +1,33 @@
 
 export const utils = {
+        
         arraysAreEqual(arr1, arr2){
 
-        if(arr1.length !== arr2.length)
-            return false
-
-        for(let i = 0; i < arr1.length; i++){
-            if(arr1[i] !== arr2[i])
+            if(arr1.length !== arr2.length)
                 return false
+
+            for(let i = 0; i < arr1.length; i++){
+                if(arr1[i] !== arr2[i])
+                    return false
+            }
+
+            return true
+        },
+
+        isPointInsideRectangle(rectangle, pointX, pointY){
+            const {x, y, width, height} = rectangle;
+            let x2 = x + width;
+            let y2 = y + height;
+            let isInsideXBoundarie = false
+            let isInsideYBoundarie = false
+            if(pointX >= Math.min(x,x2) && pointX <= Math.max(x,x2))
+                isInsideXBoundarie = true
+            if(pointY >= Math.min(y,y2) && pointY <= Math.max(y,y2))
+                isInsideYBoundarie = true
+            return isInsideXBoundarie && isInsideYBoundarie
         }
 
-        return true
-    }
+
 }
 
 export default utils;
