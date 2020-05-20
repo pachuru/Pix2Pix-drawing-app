884fab8f956c44dfc5634ffff668a57041758fb5
alephshahor <adrianalvarezleon@gmail.com>
2020-05-20 20:20:54 +0100

    If a component doesn't have an specific toolFunction is passes an empty one

--- client/src/components/toolButtonList.jsx
+++ client/src/components/toolButtonList.jsx
@@ -10,9 +10,13 @@ export default function ToolButtonList (props) {
       <ul className="tool-button-unordered-list">
         {
           toolList.map((toolName, index) => {
+
+            const toolFunction = props[toolName]
+            console.log("tool function is: ", toolFunction)
+
             return <li className="tool-button-list-element" key={index}>
               <ToolButton toolName={toolName}
-                toolFunction={props[toolName]}
+                toolFunction={toolFunction == undefined ? function(){} : toolFunction}
                 selectedTool={props.selectedTool}
                 changeSelectedTool={props.changeSelectedTool}/>
             </li>
