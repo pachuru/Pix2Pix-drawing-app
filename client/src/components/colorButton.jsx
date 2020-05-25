753e70bb2068e9699d0e0139de838224824d1c6a
alephshahor <adrianalvarezleon@gmail.com>
2020-05-25 18:07:37 +0100

    Color button highlights when selected

--- client/src/components/colorButton.jsx
+++ client/src/components/colorButton.jsx
@@ -8,12 +8,14 @@ title="Vectors Market">Vectors Market</a> from <a href="https://www.flaticon.com
 export default function ColorButton (props) {
   const colorName = props.colorName
   const buttonId = 'color-button-' + colorName.replace(/\s+/g, '-').toLowerCase()
+  const backgroundColor = (props.colorCode === props.selectedColor) ? '#1d2124' : '#343a40'
+
   return (
-    <div className="color-button-wrapper" id={buttonId} >
-      <button className="color-button" type="button" onClick={() => props.changeSelectedColor(props.colorCode)}>
+    <div className="color-button-wrapper" id={buttonId} style={{ backgroundColor: backgroundColor }} >
+      <button className="color-button" type="button" style={{ backgroundColor: backgroundColor }} onClick={() => props.changeSelectedColor(props.colorCode)}>
         <span className="color-text">{colorName}</span>
       </button>
-      <img className="color-image" src={require('../images/colors/' + colorName + '.svg')} alt="Color"/>
+      <img className="color-image" src={require('../images/colors/' + colorName + '.svg')} style={{ backgroundColor: backgroundColor }} alt="Color"/>
     </div>
   )
 }
@@ -21,5 +23,6 @@ export default function ColorButton (props) {
 ColorButton.propTypes = {
   colorName: PropTypes.string,
   colorCode: PropTypes.string,
-  changeSelectedColor: PropTypes.func
+  changeSelectedColor: PropTypes.func,
+  selectedColor: PropTypes.string
 }
