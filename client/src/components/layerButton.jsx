567b19830893be09d627db9c69ef1d679231dc4e
alephshahor <adrianalvarezleon@gmail.com>
2020-05-27 20:22:35 +0100

    Added comments to LayerButton component

--- client/src/components/layerButton.jsx
+++ client/src/components/layerButton.jsx
@@ -22,17 +22,33 @@ Icons made by <a href="https://icon54.com/" title="Pixel perfect">Pixel perfect<
 title="Flaticon"> www.flaticon.com</a>
  */
 
-export default class LayerButton extends React.Component {
+ /**
+ * LayerButton component renders a button that allows the user to change a layer name,
+ * order or delete it.
+ * @component
+ */
+class LayerButton extends React.Component {
+
+  /**
+   * @param {String} layerName the layer name that the button references to
+   */
     state = {
       layerName: ''
     }
 
+    /**
+     * Sets the layer name in the state with the layer name value passed as prop
+     */
     componentDidMount () {
       this.setState({
         layerName: this.props.layer.name
       })
     }
 
+    /**
+     * Updates the layer name of the state if it has changed 
+     * @param {Object} prevProps 
+     */
     componentDidUpdate (prevProps) {
       if (prevProps.layer.name !== this.props.layer.name) {
         this.setState({
@@ -41,12 +57,20 @@ export default class LayerButton extends React.Component {
       }
     }
 
+    /**
+     * Changes the layer name value of the state to match the one the user types in the input form
+     */
     handleInputChange = (e) => {
       this.setState({
         layerName: e.target.value
       })
     }
 
+    /**
+     * Changes the layer name in the app, calling the changeLayerName function passed as props
+     * when the user press enters. It pass as the new layer name value the layer name value stored in the
+     * app state.
+     */
     handleInputSubmit = (e) => {
       if (e.keyCode === 13) {
         e.preventDefault()
@@ -62,9 +86,11 @@ export default class LayerButton extends React.Component {
         <div className="layer-button-wrapper" id={`layer-button-${layerId}`}>
           <input className="layer-input-text" type="text" value={this.state.layerName} onChange={(e) => this.handleInputChange(e)} onKeyDown={(e) => this.handleInputSubmit(e)}>
           </input>
+          {/*
           <button className="hide-layer-button bg-dark">
             <img className="hide-layer-icon" src={require('../images/tools/hide.svg')} alt="AddIcon"/>
           </button>
+          */}
           <button className="delete-layer-button" onClick={() => this.props.deleteLayer(layerId)}>
             <img className="delete-layer-icon" src={require('../images/tools/remove.svg')} alt="DeleteIcon"/>
           </button>
@@ -79,6 +105,14 @@ export default class LayerButton extends React.Component {
     }
 }
 
+/**
+ * LayerButton proptypes
+ * @param {Object} layer the layer the button references to
+ * @param {Function} deleteLayer function to delete the layer
+ * @param {Function} increaseLayerOrder function to increase the layer order
+ * @param {Function} decreaseLayerOrer function to decrease the layer order
+ * @param {Function} changeLayerName function to change the layer name
+ */
 LayerButton.propTypes = {
   layer: PropTypes.object,
   deleteLayer: PropTypes.func,
@@ -86,3 +120,5 @@ LayerButton.propTypes = {
   decreaseLayerOrder: PropTypes.func,
   changeLayerName: PropTypes.func
 }
+
+export default LayerButton
