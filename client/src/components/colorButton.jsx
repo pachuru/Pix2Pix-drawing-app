2ebb3617450808f34f8b3057bf15dfbb06be1a79
alephshahor <adrianalvarezleon@gmail.com>
2020-05-27 20:54:51 +0100

    Added comments to ColorButton

--- client/src/components/colorButton.jsx
+++ client/src/components/colorButton.jsx
@@ -5,7 +5,13 @@ import PropTypes from 'prop-types'
 /* Icons made by <a href="https://www.flaticon.com/authors/vectors-market"
 title="Vectors Market">Vectors Market</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a> */
 
-export default function ColorButton (props) {
+
+/**
+ * ColorButton component returns a button that displays a color and its name.
+ * It allows the user to choose that color and set it as the currently selected color.
+ * @component
+ */
+function ColorButton (props) {
   const colorName = props.colorName
   const buttonId = 'color-button-' + colorName.replace(/\s+/g, '-').toLowerCase()
   const backgroundColor = (props.colorCode === props.selectedColor) ? '#1d2124' : '#343a40'
@@ -20,9 +26,19 @@ export default function ColorButton (props) {
   )
 }
 
+
+/**
+ * ColorButton proptypes
+ * @param {String} colorName the name of the color
+ * @param {String} colorCode the hexadecimal code of the color
+ * @param {Function} changeSelectedColor function to change the currently selected color
+ * @param {String} selectedColor the hex code of the currently selected color
+ */
 ColorButton.propTypes = {
   colorName: PropTypes.string,
   colorCode: PropTypes.string,
   changeSelectedColor: PropTypes.func,
   selectedColor: PropTypes.string
 }
+
+export default ColorButton
