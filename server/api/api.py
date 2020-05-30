2de8bd83c9c5e9394c8f6559cf5b5d1aab69bc7d
alephshahor <adrianalvarezleon@gmail.com>
2020-05-30 21:45:34 +0100

    Refactored Rest API to avoid redundant operations

--- server/api/api.py
+++ server/api/api.py
@@ -18,30 +18,41 @@ app = Flask(__name__)
 cors = CORS(app)
 api = Api(app)
 
-def processImage():
-    image = load_img('./image.png', target_size=(256,256))
-    image = img_to_array(image)
-    image = (image - 127.5) / 127.5
-    model_path = './generator_model.h5'
-    image_shape = image.shape
-    image = np.expand_dims(image, axis=0)
-    generator = model.generator(image_shape)
-    generator.load_weights(model_path)
-    fake_image = generator.predict(image)[0]
-    fake_image = (fake_image + 1) / 2.0
 
-    sizes = np.shape(fake_image)
-    fig = pyplot.figure()
-    fig.set_size_inches(1. * sizes[0] / sizes[1], 1, forward = False)
-    ax = pyplot.Axes(fig, [0., 0., 1., 1.])
-    ax.set_axis_off()
-    fig.add_axes(ax)
-    ax.imshow(fake_image)
-    pyplot.savefig('./prediction.png', dpi = sizes[0], cmap='hot') 
-    pyplot.close()
 
 
 class RestApi(Resource):
+
+    def __init__ (self):
+        image_shape = (256,256,3)
+        self.generator = model.generator(image_shape)
+        model_path = './generator_model.h5'
+        self.generator.load_weights(model_path)
+
+    def saveImage(self, fake_image):
+        sizes = np.shape(fake_image)
+        fig = pyplot.figure()
+        fig.set_size_inches(1. * sizes[0] / sizes[1], 1, forward = False)
+        ax = pyplot.Axes(fig, [0., 0., 1., 1.])
+        ax.set_axis_off()
+        fig.add_axes(ax)
+        ax.imshow(fake_image)
+        pyplot.savefig('./prediction.png', dpi = sizes[0], cmap='hot') 
+        pyplot.close()
+
+
+    def processImage(self):
+        image = load_img('./image.png', target_size=(256,256))
+        image = img_to_array(image)
+        image = (image - 127.5) / 127.5
+        image_shape = image.shape
+        image = np.expand_dims(image, axis=0)
+        fake_image = self.generator.predict(image)[0]
+        fake_image = (fake_image + 1) / 2.0
+        self.saveImage(fake_image)
+
+
+
     def post(self):
         image_b64 = request.get_json()['data']
         image_data = re.sub('^data:image/.+;base64,', '', image_b64)
@@ -49,7 +60,7 @@ class RestApi(Resource):
         buf = io.BytesIO(image_data)
         image_PIL = Image.open(buf)
         image_PIL.save('./image.png', quality=100)
-        processImage()
+        self.processImage()
         encoded_prediction = base64.b64encode(open("./prediction.png", "rb").read())
         encoded_prediction = re.sub('b\'', '', str(encoded_prediction))
         return jsonify({"encoded_prediction": str(encoded_prediction)})
